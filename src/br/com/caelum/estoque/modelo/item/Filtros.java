153d082f6faf992fa0eaa6d7022e135381b9b0ea
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-07-06 22:20:27 -0400

    O primeiro serviço SOAP

---
+++ src/br/com/caelum/estoque/modelo/item/Filtros.java
@@ -0,0 +1,25 @@
+package br.com.caelum.estoque.modelo.item;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+public class Filtros {
+
+	private List<Filtro> filtros;
+
+	public Filtros(List<Filtro> filtros) {
+		this.filtros = filtros;
+	}
+
+	Filtros() {
+	}
+
+	public List<Filtro> getLista() {
+		return filtros;
+	}
+
+}
---
+++ src/br/com/caelum/estoque/modelo/item/TipoItem.java
@@ -0,0 +1,30 @@
+package br.com.caelum.estoque.modelo.item;
+
+import javax.xml.bind.annotation.XmlEnumValue;
+
+public enum TipoItem {
+
+	LIVRO("Livro"), 
+	CELULAR("Celular"),
+	TABLET("Tablet");
+	
+	private String nome;
+
+	TipoItem(String nome) { 
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public static boolean existe(String valor) {
+		try{
+			//joga IllegalArgumentException caso nao exista
+			TipoItem.valueOf(valor.toUpperCase());
+		}catch(IllegalArgumentException e) {
+			return false;
+		}
+		return true;
+	}
+}
