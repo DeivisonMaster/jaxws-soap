ad6cba55702711d5f82e5793972c51594343a85c
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-07-08 18:47:18 -0400

    Personalizando o serviço

--- src/br/com/caelum/estoque/modelo/item/ListaItens.java
+++ src/br/com/caelum/estoque/modelo/item/ListaItens.java
@@ -7,14 +7,18 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement // define a classe como elemento raiz do XML
+@XmlAccessorType(XmlAccessType.FIELD) // define o tipo de acesso por atributo
 public class ListaItens {
 
+	@XmlElement(name = "item") // define o nome de um elemento ou tag do XML
 	private List<Item> itens;
 
 	public ListaItens(List<Item> itens) {
 		this.itens = itens;
 	}
 
+	// JAX-B requer o uso de construtor sem argumentos
 	ListaItens() {
 	}
 
--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -2,22 +2,28 @@ package br.com.caelum.estoque.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
+	private static int contador = 0;
 	
 	
-	public List<Item> getItens(){
-		
-		System.out.println("Chamando getItens");
+	@WebMethod(operationName = "todosOsItens") // define um novo para o metodo
+	@WebResult(name = "itens")	// renderiza nome para retorno do metodo
+	public ListaItens getItens(){
+		contador++;
+		System.out.println("Chamando getItens" + contador);
 		
 		List<Item> lista = dao.todosItens();
-		return lista;
+		return new ListaItens(lista);
 	}
 }
