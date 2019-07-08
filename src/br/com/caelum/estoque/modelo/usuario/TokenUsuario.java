a90c7361e83973a33999f36661540b73b7c38f22
deivisonmaster <deivisonoliveira.info@gmail.com>
2019-07-08 19:36:55 -0400

    Trabalhando com cabeçalhos

--- src/br/com/caelum/estoque/modelo/usuario/TokenUsuario.java
+++ src/br/com/caelum/estoque/modelo/usuario/TokenUsuario.java
@@ -6,13 +6,10 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
-@XmlAccessorType(XmlAccessType.FIELD)
 public class TokenUsuario {
 
-    @XmlElement(required=true)
 	private String token;
     
-    @XmlElement(required=true)
 	private Date dataValidade;
 	
 	//JAX-B precisa desse construtor
--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,12 +3,19 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
+import br.com.caelum.estoque.modelo.usuario.Usuario;
 
 @WebService
 public class EstoqueWS {
@@ -17,13 +24,46 @@ public class EstoqueWS {
 	private static int contador = 0;
 	
 	
-	@WebMethod(operationName = "todosOsItens") // define um novo para o metodo
+	@WebMethod(operationName = "TodosOsItens") // define um novo para o metodo
 	@WebResult(name = "itens")	// renderiza nome para retorno do metodo
-	public ListaItens getItens(){
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros){
 		contador++;
 		System.out.println("Chamando getItens" + contador);
 		
-		List<Item> lista = dao.todosItens();
+		List<Filtro> listaFiltros = filtros.getLista();
+		List<Item> lista = dao.todosItens(listaFiltros);
+		
 		return new ListaItens(lista);
 	}
+	
+	
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) 
+			TokenUsuario tokenUsuario, @WebParam(name = "item") Item item) 
+					throws AutorizacaoException {
+		
+		System.out.println("Cadastrando item " + item + ", " + "Token " + tokenUsuario);
+		
+		boolean isValido = new TokenDao().ehValido(tokenUsuario);
+		
+		if(!isValido) {
+			throw new AutorizacaoException("Autorizacao falhou");
+		}
+		
+		this.dao.cadastrar(item);
+		
+		return item;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
